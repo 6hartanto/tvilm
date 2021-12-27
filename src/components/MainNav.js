b9b5c9110310c540f2ff3a7c99c5d49928e1a0d3
6hartanto <6hartanto@gmail.com>
2021-12-27 15:35:29 +0700

    implement link in bottom nav

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
@@ -5,6 +6,7 @@ import TvIcon from "@material-ui/icons/Tv";
 import MovieIcon from "@material-ui/icons/Movie";
 import SearchIcon from "@material-ui/icons/Search";
 import WhatshotIcon from "@material-ui/icons/Whatshot";
+import { useNavigate } from "react-router-dom";
 
 const useStyles = makeStyles({
   root: {
@@ -18,9 +20,27 @@ const useStyles = makeStyles({
 
 export default function SimpleBottomNavigation() {
   const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (value === 0) {
+      navigate("/");
+    } else if (value === 1) {
+      navigate("/movies");
+    } else if (value === 2) {
+      navigate("/series");
+    } else if (value === 3) {
+      navigate("/search");
+    }
+  }, [value, navigate]);
 
   return (
     <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
       showLabels
       className={classes.root}
     >
